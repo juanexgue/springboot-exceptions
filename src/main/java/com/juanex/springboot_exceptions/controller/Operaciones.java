edf9317d247340f9043248044bf5898cc5c0e62a
Juan Guevara M. <juanexgue@gmail.com>
2025-07-26 20:56:04 -0500

    Exceptions Arithmetic NumberFormat

--- src/main/java/com/juanex/springboot_exceptions/controller/Operaciones.java
+++ src/main/java/com/juanex/springboot_exceptions/controller/Operaciones.java
@@ -1,6 +1,7 @@
 package com.juanex.springboot_exceptions.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,9 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Operaciones {
 
     @GetMapping
-    public String Divide() {
-        int valor = 20 / 0;
-        return "resultado: " + valor;
-    }
+    public String Divide(@RequestParam(name = "numerador") String numeradorStr,
+            @RequestParam(name = "denominador") String denominadorStr) {
+        try {
+            int numerador = Integer.parseInt(numeradorStr);
+            int denominador = Integer.parseInt(denominadorStr);
+            int resultado = numerador / denominador;
+            return "Resultado: " + resultado;
+        } catch (ArithmeticException e) {
+            throw e;
+        } catch (NumberFormatException e) {
+            throw e;
+        }
 
+    }
 }
--- src/main/java/com/juanex/springboot_exceptions/handler/ManejoErrores.java
+++ src/main/java/com/juanex/springboot_exceptions/handler/ManejoErrores.java
@@ -1,8 +1,20 @@
 package com.juanex.springboot_exceptions.handler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ManejoErrores {
 
+    @ExceptionHandler(ArithmeticException.class)
+    public ResponseEntity<String> handleArithmeticException(ArithmeticException ex) {
+        return new ResponseEntity<>("Error: División por cero no permitida", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    public ResponseEntity<String> handleNumberFormatException(NumberFormatException ex) {
+        return new ResponseEntity<>("Error: El valor ingresado no es numérico", HttpStatus.BAD_REQUEST);
+    }
 }
